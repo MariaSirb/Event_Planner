8841a8d4da6003f926a27e2b9c5e95b13c05ee3a
MariaSirb <sirbmaria27@gmail.com>
2023-02-16 18:22:14 +0200

    Incercare pagina Evenimente

---
+++ Pages/Evenimente.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Event_Planner.Pages
+{
+    public class EvenimenteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
